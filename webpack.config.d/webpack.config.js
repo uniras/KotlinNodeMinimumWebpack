36772e818fb92cffcfbb74f3f7a8e0331a375549
RiaFeed <tkappeng@gmail.com>
2022-11-05 21:26:25 +0900

    Add files via upload

--- webpack.config.d/webpack.config.js
+++ webpack.config.d/webpack.config.js
@@ -1,5 +1,6 @@
 var nodeExternals = require('webpack-node-externals');
 config.target = 'node';
+config.mode = 'production';
 config.externalsPresets = {node: true}
 config.externals = [nodeExternals({modulesDir:__dirname + '\\..\\..\\node_modules'})];
 
@@ -8,3 +9,6 @@ config.externals = [nodeExternals({modulesDir:__dirname + '\\..\\..\\node_module
 
 //minifyを無効にする場合はこうする
 //config.optimization = {minimize: false}
+
+//SourceMapを生成しないようにするにはこうする
+//config.devtool = false;
